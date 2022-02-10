09641d07d87d6664c67f76550362d05b64382aaa
jwheeler0424 <jonathan.wheeler87@gmail.com>
2022-02-09 16:29:30 -0800

    day 2 video 23 interactive coding exercise bmi

---
+++ day 2/vid22.py
@@ -0,0 +1,18 @@
+# Day 2 video 22
+
+3 + 5   # add
+7 - 4   # subtract
+3 * 2   # multiply
+6 / 3   # divide
+
+2 ** 8  # exponential powers
+
+print (2 ** 8)
+
+# PEDMAS L->R
+# ()
+# **
+# * /
+# + -
+
+print(3 * 3 + 3 / 3 - 3)
