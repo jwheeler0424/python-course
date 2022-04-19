7dfd4e553a270c1e9e70199ac44144f222076476
jwheeler0424 <jonathan.wheeler87@gmail.com>
2022-04-19 16:22:55 -0700

    day 4 video 47

--- day 4/vid47_project_rpc.py
+++ day 4/vid47_project_rpc.py
@@ -32,38 +32,21 @@ rps = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 computer_choice = random.randint(0, 2)
 
-print(rps[user_choice])
-print("Computer chose:")
-print(rps[computer_choice])
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you lose!")
+else:
+    print(rps[user_choice])
+    print("Computer chose:")
+    print(rps[computer_choice])
 
-if user_choice == 0:
-    if computer_choice == 0:
-        print("It's a draw.")
-        
-    elif computer_choice == 1:
-        print("You lose.")
-        
-    else:
+
+    if user_choice == 0 and computer_choice == 2:
         print("You win!")
-        
-    
-elif user_choice == 1:
-    if computer_choice == 0:
-        print("You win!")
-        
-    elif computer_choice == 1:
-        print("It's a draw.")
-        
-    else:
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose.") 
+    elif computer_choice > user_choice:
         print("You lose.")
-        
-elif user_choice == 2:
-    if computer_choice == 0:
-        print("You lose.")
-        
-    elif computer_choice == 1:
-        print("You win!")
-        
-    else:
+    elif user_choice > computer_choice:
+        print("You win!") 
+    elif computer_choice == user_choice:
         print("It's a draw.")
-        
